a334823036c1c7b374ca79810e784da25b125523
Indrek <leede.indrek@gmail.com>
2021-06-28 13:31:29 +0200

    Added rules pages for all categories

---
+++ src/wiki/hell-rules.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-row dense>
+      <!-- Title -->
+
+      <v-col cols="12">
+        <h1>Hell category rules</h1>
+        <v-card>
+          <v-card-title>
+            Category specific
+          </v-card-title>
+          <v-card-text>
+            Timer starts right after character creation and ends when you land
+            the killing blow on hell Baal.
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            General rules
+          </v-card-title>
+          <v-card-text>
+            <ul>
+              <li>
+                Patch 1.14d
+              </li>
+              <li>
+                -w -sndbkg -3dfx are allowed
+              </li>
+              <li>-act -ns -direct -txt -ns are banned</li>
+            </ul>
+            <ul>
+              <li>
+                Using TCP/IP Host is allowed to reroll maps, but interacting
+                with other characters in a TCP/IP game is banned
+              </li>
+              <li>Joining a TCP/IP game or using Open Battle.net is banned</li>
+              <li>
+                Hacks, mods or map revealing tools are banned. Use of Diablo
+                Interface during a run is allowed.
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
---
+++ src/wiki/twink-rules.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container>
+    <v-row dense>
+      <!-- Title -->
+
+      <v-col cols="12">
+        <h1>Twink category rules</h1>
+        <v-card>
+          <v-card-title>
+            Category specific
+          </v-card-title>
+          <v-card-text>
+            Timer starts right after your character loads into the game and ends
+            when all quests, cow kings and level 80 is completed.
+          </v-card-text>
+          <v-card-text>
+            <ul>
+              <li>
+                You CANNOT start with your character pre-equipped. All items
+                must start from your inventory/stash. The only exception is for
+                the character's original starting items as mentioned above.
+              </li>
+              <li>
+                Unseeded maps only. If you reset your run, you should reroll
+                your map seed using something like TCP/IP
+              </li>
+            </ul>
+          </v-card-text>
+          <v-card-text>
+            <ul>
+              <li>
+                Complete all quests in all difficulties.
+              </li>
+              <li>
+                Kill all Cow Kings.
+              </li>
+              <li>
+                Reach level 80.
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            Starting items
+          </v-card-title>
+          <v-card-text>
+            <ul>
+              <li>
+                Start with any non-quest items you want in your stash/inventory,
+                limited to: Helms, Weapons, Shields, Chests, Gloves, Boots,
+                Belts, Rings, Amulets, Throwing Potions
+              </li>
+              <li>
+                That means that you can socket runes/gems/jewels into your gear
+                but, you can't start with free floating runes/gems/jewels
+              </li>
+              <li>
+                Starting with gold not allowed, but you can sell your items for
+                gold.
+              </li>
+              <li>Ladder items are allowed.</li>
+              <li>
+                You're allowed to only have one item with teleport charges.
+              </li>
+              <li>
+                All items must be a legitimate item that you could potentially
+                find/craft playing vanilla Diablo II: LOD 1.14d. To check if
+                item is legitimate use this tool:
+                <a
+                  href="https://tools.harrogath.com/affixcalc.html#"
+                  target="_blank"
+                  >Affix Calc</a
+                >
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            General rules
+          </v-card-title>
+          <v-card-text>
+            <ul>
+              <li>
+                Patch 1.14d.
+              </li>
+              <li>
+                -w -sndbkg -3dfx are allowed.
+              </li>
+              <li>-act -ns -direct -txt -ns are banned.</li>
+            </ul>
+            <ul>
+              <li>
+                Using TCP/IP Host is allowed to reroll maps, but interacting
+                with other characters in a TCP/IP game is banned.
+              </li>
+              <li>Joining a TCP/IP game or using Open Battle.net is banned.</li>
+              <li>
+                Hacks, mods or map revealing tools are banned. Use of Diablo
+                Interface during a run is allowed.
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
